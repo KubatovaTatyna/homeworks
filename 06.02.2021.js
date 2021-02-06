85344065508f22d288c442701c3ddc391d040357
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-07 00:04:15 +0600

    Created button list in js

--- 06.02.2021.js
+++ 06.02.2021.js
@@ -1,3 +1,15 @@
 let h1 = document.createElement("h1");
 h1.innerText = "0";
 document.body.append(h1);
+
+let ul = document.createElement("ul");
+for (let i = 1; i <= 5; i++) {
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = "Example " + "#" +i;
+    button.id = "example" + i;
+
+    li.append(button);
+    ul.append(li);
+}
+document.body.append(ul);
