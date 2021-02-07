bcde794eef526057a9b5eef5cd87b0a5fcd99663
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-07 12:18:34 +0600

    Created first button 'random color of h1'

--- 06.02.2021.js
+++ 06.02.2021.js
@@ -13,3 +13,34 @@ for (let i = 1; i <= 5; i++) {
     ul.append(li);
 }
 document.body.append(ul);
+
+// example1.addEventListener("click" , function(){
+//     for (let increse = 0; increse < array.length; increse++) {
+//         const element = array[increse];
+        
+//     }
+// })
+// if (example1.addEvenListenner("click" , function(){
+//     for (let increase = 1; increase < 10; increase++) {
+//         h1.innerText = increase;
+//     }
+// })) {
+    
+// }
+// example2.addEventListener("click" , function(){ 
+//     for (let increase = 0; increase <= 10; increase++) {
+//         h1.innerText = increase;
+//     }
+// });
+// example3.addEventListener("click" , function(){
+//     for (let size = 0; size <= 5; size++) {
+//         h1.style.fontSize = size;
+//     }
+// })
+
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    h1.style.color = "#" + randomColor;
+  }
+  example1.addEventListener("click", setBg);
+setBg();
