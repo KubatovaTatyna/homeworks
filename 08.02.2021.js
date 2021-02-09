698aa025938c055189b7507da0405711ec718512
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-09 21:31:04 +0600

    toFixed the random nuber in

--- 08.02.2021.js
+++ 08.02.2021.js
@@ -1,17 +1,26 @@
 let button = document.querySelector('button');
 let password = document.querySelector("#password");
-let p = document.createElement("p");
-let p1 = document.createElement("p")
+
+let plusButton = document.querySelector("#plus");
+let minusButton = document.querySelector("#minus");
+let multiplicationButton = document.querySelector("#multiplication");
+let divisionButton = document.querySelector("#division");
+
 let numberInput = document.querySelector("#number");
+let numberInput2 = document.querySelector("#number-2");
+let numberInput3 = document.querySelector("#number-3");
+
+let p = document.createElement("p");
+let p1 = document.createElement("p");
+let p2 = document.createElement("p");
+
 p.innerText = "";
 p1.innerText = "";
+p2.innerText = "";
+
 document.body.append(p);
 document.body.append(p1);
-
-button.addEventListener('click', function(event) {
-    console.log(event);
-    event.preventDefault();
-});
+document.body.append(p2);
 
 password.addEventListener('input' , function(event){
     if(password.value.length <= 5){
@@ -26,12 +35,17 @@ password.addEventListener('input' , function(event){
 })
 
 let randomNumber = Math.random() * 6 ;
+randomNumber.toFixed;
 numberInput.addEventListener('input' , function(event){
     if(numberInput.value > randomNumber){
-        p1.innerText = "You win!"
+        p1.innerText = "You win! The number is " + randomNumber.toFixed();
     }
     else{
-        p1.innerText = "You lost!"
+        p1.innerText = "You lost! The number is " + randomNumber.toFixed();
     }
 })
 console.log(randomNumber);
+
+// plusButton.addEventListener('click' , function(event){
+//     p2.innerText = numberInput2.value + numberInput3.value;
+// });
