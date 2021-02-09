9eb9344694c327150c3e1cf24f7af136f0397163
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-09 22:43:48 +0600

    Created compare button

--- 08.02.2021.js
+++ 08.02.2021.js
@@ -6,26 +6,33 @@ let minusButton = document.querySelector("#minus");
 let multiplicationButton = document.querySelector("#multiplication");
 let divisionButton = document.querySelector("#division");
 let degreeButton = document.querySelector("#degree");
+let compareButton = document.querySelector("#compare");
 
-let underText = document.querySelector("#under-text")
-let underText2 = document.querySelector("#under-text2")
-let underText3 = document.querySelector("#under-text3")
+let underText = document.querySelector("#under-text");
+let underText2 = document.querySelector("#under-text2");
+let underText3 = document.querySelector("#under-text3");
+let underText4 = document.querySelector("#under-text4");
 
 let numberInput = document.querySelector("#number");
 let numberInput2 = document.querySelector("#number-2");
 let numberInput3 = document.querySelector("#number-3");
+let numberInput4 = document.querySelector("#number-4");
+let numberInput5 = document.querySelector("#number-5");
 
 let p = document.createElement("p");
 let p1 = document.createElement("p");
 let p2 = document.createElement("p");
+let p3 = document.createElement("p");
 
 p.innerText = "";
 p1.innerText = "";
 p2.innerText = "";
+p3.innerText = "";
 
 underText.append(p);
 underText2.append(p1);
 underText3.append(p2);
+underText4.append(p3);
 
 password.addEventListener('input' , function(event){
     if(password.value.length <= 5){
@@ -37,7 +44,7 @@ password.addEventListener('input' , function(event){
     else if(password.value.length  >= 10){
         p.innerText = "Excellent!"
     }
-})
+});
 
 let randomNumber = Math.random() * 6 ;
 randomNumber.toFixed;
@@ -48,7 +55,7 @@ numberInput.addEventListener('input' , function(event){
     else{
         p1.innerText = "You lost! The number is " + randomNumber.toFixed();
     }
-})
+});
 console.log(randomNumber);
 
 plusButton.addEventListener('click' , function(event){
@@ -70,4 +77,16 @@ divisionButton.addEventListener('click' , function(event){
 degreeButton.addEventListener('click' , function(event){
     let degree = numberInput2.value ** numberInput3.value;
     p2.innerText = degree;
+});
+
+compareButton.addEventListener("click", function(event){
+    if(numberInput4 > numberInput5){
+        p3.innerText = "a > b"
+    }
+    else if(numberInput4 < numberInput5){
+        p3.innerText = "a < b"
+    }
+    else{
+        p3.innerText = "a = b"
+    }
 });
