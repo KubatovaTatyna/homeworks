d580822add3bff27d12dbac137b6659baa9e282b
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-11 23:12:24 +0600

    Added event listener 'mousedown'

--- mouse-events/scripts.js
+++ mouse-events/scripts.js
@@ -1,1 +1,6 @@
 let div = document.querySelector('#example');
+document.body.addEventListener('mousedown', function(event){
+    console.log(event.clientY , event.clientX);
+    div.style.top = event.clientY + "px";
+    div.style.left = event.clientX + "px";
+});
