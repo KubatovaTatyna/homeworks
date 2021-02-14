7d9312fec1be797cc4cddc080ded77dff51f1203
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-14 12:32:46 +0600

    Styled this div

--- 14.02.2021.js
+++ 14.02.2021.js
@@ -1,3 +1,7 @@
 document.body.addEventListener('mousedown' , function(event){
     let div = document.createElement('div');
+    div.style.borderRadius = '50%';
+    div.style.backgroundColor = 'red';
+    div.style.width = '30px';
+    div.style.height = '30px';
 });
