71565ff8487ec23ddec98cf4fe55e5004b123b45
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-14 12:37:42 +0600

    Added div position in body

--- 14.02.2021.js
+++ 14.02.2021.js
@@ -4,6 +4,11 @@ document.body.addEventListener('mousedown' , function(event){
     div.style.backgroundColor = 'red';
     div.style.width = '30px';
     div.style.height = '30px';
-    
+
     document.body.append(div);
+
+    div.style.position = "absolute";
+    div.style.top = event.clientY - 15 + "px" ;
+    div.style.left = event.clientX - 15 + "px";
+
 });
