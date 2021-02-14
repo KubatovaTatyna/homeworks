67a5e7fd126c56f21116719bcb5b15dd8328ffb5
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-14 12:41:52 +0600

    Added new event listener and numbers key

--- 14.02.2021.js
+++ 14.02.2021.js
@@ -18,7 +18,7 @@ document.body.addEventListener('mousedown' , function(event){
             div.style.backgroundColor = 'blue';
             event.preventDefault();
     }
-    
+
     if (event.ctrlKey) {
         div.style.borderRadius = '0';
         }
@@ -32,4 +32,37 @@ document.body.addEventListener('mousedown' , function(event){
             div.style.width = '60px';
             div.style.height = '30px';
         }
+        
+    document.body.addEventListener('keydown' , function(event){
+        if(event.which == 49){
+            div.style.opacity = .1;
+        }
+        else if(event.which == 50){
+            div.style.opacity = .2;
+        }
+        else if(event.which == 51){
+            div.style.opacity = .3;
+        }
+        else if(event.which == 52){
+            div.style.opacity = .4;
+        }
+        else if(event.which == 53){
+            div.style.opacity = .5;
+        }
+        else if(event.which == 54){
+            div.style.opacity = .6;
+        }
+        else if(event.which == 55){
+            div.style.opacity = .7;
+        }
+        else if(event.which == 56){
+            div.style.opacity = .8;
+        }
+        else if(event.which == 57){
+            div.style.opacity = .9;
+        }
+        else if(event.which == 48){
+            div.style.opacity = .09;
+        }
+    });
 });
